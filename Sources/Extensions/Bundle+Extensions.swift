4379e82a4eaf07ed930333550cec1347eac12e69
Caner Y. Tatar <caner.tatar@depop.com>
2022-02-08 11:38:26 +0000

    Bundle.module line is removed due to lack of resourced

--- Sources/Extensions/Bundle+Extensions.swift
+++ Sources/Extensions/Bundle+Extensions.swift
@@ -25,9 +25,6 @@
 import Foundation
 
 internal extension Bundle {
-    #if IS_SPM
-    static var messageKitAssetBundle: Bundle = Bundle.module
-    #else
     static var messageKitAssetBundle: Bundle {
         return Bundle(for: MessagesViewController.self)
     }
